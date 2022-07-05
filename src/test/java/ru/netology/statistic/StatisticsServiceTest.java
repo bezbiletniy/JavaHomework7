a8de9f21a0ca44b10178cf1524aff8dcfc133319
bezbiletniy <a.borodin.ca@gmail.com>
2022-07-05 23:08:06 +0300

    Add Tests

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,29 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxNewArray() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 0, 8, 13, 5, 3, 8, 6, 11, 11, 4};
+        long expected = 13;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxWithNegative() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 0, 8, -1, 5, 3, 8, 6, 11, 11, 4};
+        long expected = 11;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
 }
